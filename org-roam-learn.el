70d990ae2658ee49f30c91b9666465a4c18bee8c
Domse007 <dominikkeller55@gmail.com>
2023-03-04 19:03:20 +0100

    Fixed errors.

--- org-roam-learn.el
+++ org-roam-learn.el
@@ -1,3 +1,5 @@
+;; (org-end-of-subtree)
+
 (require 'org-roam)
 
 (defvar org-roam-learn-overlays '()
@@ -9,20 +11,29 @@
 (defun org-roam-learn-node (node)
   "Learn a NODE. This will hide the buffer contents and only show the title of
 the NODE. To reveal the node use `org-roam-learn-reveal'."
-  (interactive)
   (org-roam-node-visit node)
-  (let ((dest-pos))
+  (let ((dest-pos)
+        (node-name (org-roam-node-title node))
+        (node-pos (org-roam-node-point node)))
     (save-excursion
-      (goto-char (point-min))
-      (setq dest-pos (re-search-forward "#\\+\\(TITLE\\|Title\\|title\\):"
-					nil t))
-      (goto-char dest-pos)
+      (goto-char node-pos)
+      (goto-char (re-search-forward node-name))
       (next-line)
+      ;; (goto-char (point-min))
+      ;; (setq dest-pos (re-search-forward "#\\+\\(TITLE\\|Title\\|title\\):"
+      ;;   				nil t))
+      ;; (goto-char dest-pos)
       (beginning-of-line)
-      (let ((overlay (make-overlay (point) (point-max) (current-buffer))))
-	(overlay-put overlay 'display "...")
-	(push overlay org-roam-learn-overlays)))
-    (goto-char (1+ dest-pos))))
+      (save-excursion
+        (let* ((start-pos (point))
+               (end-pos (progn (org-end-of-subtree)
+                               (point)))
+               (overlay (make-overlay start-pos end-pos (current-buffer))))
+          (setq dest-pos start-pos)
+          (overlay-put overlay 'display "...")
+          (push overlay org-roam-learn-overlays))))
+    (goto-char (1+ dest-pos))
+    (recenter-top-bottom 1)))
 
 (defun org-roam-learn-reveal ()
   "Remove the overlay from the current buffer."
@@ -42,24 +53,25 @@ the NODE. To reveal the node use `org-roam-learn-reveal'."
 
 (defun org-roam-learn-select-random-node (nodes)
   "Take a random node from NODES and return it."
-  (cdr (seq-random-elt (nodes))))
+  (seq-random-elt nodes))
 
 (defun org-roam-learn-matches-tag (node tag)
   "Check if NODE has TAG as one of it's members."
   (member tag (org-roam-node-tags node)))
 
 (defun org-roam-learn-get-nodes-matching-tag (tag)
-  (let ((nodes (org-roam-node-list))
-	(nodes (cl-remove-if-not
-		(lambda (node) (org-roam-learn-matches-tag node tag)))))
-    nodes))
+  (let* ((nodes (org-roam-node-list))
+	 (matched-nodes (cl-remove-if-not
+		         (lambda (node) (org-roam-learn-matches-tag node tag))
+                         nodes)))
+    matched-nodes))
 
 (defun org-roam-learn ()
   "Select a tag and learn a random node."
   (interactive)
-  (let ((tag (completing-read "Tag: " org-roam-registered-tags nil t))
-	(nodes (org-roam-learn-get-nodes-matching-tag tag))
-	(result (org-roam-learn-select-random-node nodes)))
+  (let* ((tag (completing-read "Tag: " org-roam-registered-tags nil t))
+	 (nodes (org-roam-learn-get-nodes-matching-tag tag))
+	 (result (org-roam-learn-select-random-node nodes)))
     (org-roam-learn-node result)))
 
 (defun org-roam-learn-random-by-tag (tag)
